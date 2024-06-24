5c0d2bb09a58b7747ca98bd95957b2bb3c13db24
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-23 18:31:23 -1000

    i always hated that the colors were slightly desaturated. all fixed now!

--- src/main/java/io/github/arrayv/visuals/Visual.java
+++ src/main/java/io/github/arrayv/visuals/Visual.java
@@ -20,7 +20,7 @@ public abstract class Visual {
     }
 
     public static Color getIntColor(int i, int length) {
-        return Color.getHSBColor(((float) i / length), 0.8F, 0.8F);
+        return Color.getHSBColor(((float) i / length), 1F, 1F);
     }
 
     public static void markBar(Graphics2D bar, boolean color, boolean rainbow, boolean analysis) {
