b7a10930b5b386b14e729782b84b798927c80131
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-23 18:10:25 -1000

    Create BosoSort.java

---
+++ src/main/java/io/github/arrayv/sorts/distribute/BosoSort.java
@@ -0,0 +1,21 @@
+ public final class BosoSort extends BogoSorting {
+   public BosoSort(ArrayVisualizer arrayVisualizer) {
+     super(arrayVisualizer);
+     this.setSortListName("Boso");
+     this.setRunAllSortsName("Boso Sort (Stupid Bubble Bogo Sort)");
+     this.setRunSortName("Boso Sort");
+     this.setCategory("Bogo Sorts");
+     this.setComparisonBased(true);
+     this.setBucketSort(false);
+     this.setRadixSort(false);
+     this.setUnreasonablySlow(true);
+     this.setUnreasonableLimit(32);
+     this.setBogoSort(true);
+   }
+   public void runSort(int[] array, int length, int bucketCount) {
+     while (!this.isArraySorted(array, length)) {
+       i = BogoSorting.randInt(0, length-1);
+         Writes.swap(array, i, i+1, this.delay, true, false);
+   }
+ }
+ }
