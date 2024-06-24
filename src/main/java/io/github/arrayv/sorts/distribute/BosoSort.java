98bd20e92432c20b8459eb6c11e54d66d2765f01
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-23 19:43:10 -1000

    highlight change

--- src/main/java/io/github/arrayv/sorts/distribute/BosoSort.java
+++ src/main/java/io/github/arrayv/sorts/distribute/BosoSort.java
@@ -1,9 +1,10 @@
-package io.github.arrayv.sorts.distribute;
+ package io.github.arrayv.sorts.distribute;
 
 import io.github.arrayv.main.ArrayVisualizer;
 import io.github.arrayv.sorts.templates.BogoSorting;
 
 //bubble bogo sort but stupider, picks a random item from 0 to n-1 and swaps it with the subsequent item. Unconditionally. Checks if array = sorted. If no, do it again.
+ 
  public final class BosoSort extends BogoSorting {
    public BosoSort(ArrayVisualizer arrayVisualizer) {
      super(arrayVisualizer);
@@ -21,7 +22,11 @@ import io.github.arrayv.sorts.templates.BogoSorting;
    public void runSort(int[] array, int length, int bucketCount) {
      while (!this.isArraySorted(array, length)) {
        int i = BogoSorting.randInt(0, length-1);
-         Writes.swap(array, i, i+1, this.delay, true, false);
+	   int highlight = BogoSorting.randomInt(0, length-1);
+       Writes.swap(array, i, i+1, this.delay, false, false);
+	   Highlights.markArray(1, highlight)
+	   Highlights.markArray(2, highlight + 1)
+		 
    }
  }
  }
