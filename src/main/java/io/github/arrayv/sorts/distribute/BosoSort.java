9c0606f7e07fd92197b881e6ceb59e30389f5831
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-23 18:42:35 -1000

    IMPORT OOPSIE DROOPSIES I FORGOT LE IMPORTS

--- src/main/java/io/github/arrayv/sorts/distribute/BosoSort.java
+++ src/main/java/io/github/arrayv/sorts/distribute/BosoSort.java
@@ -1,3 +1,9 @@
+package io.github.arrayv.sorts.distribute;
+
+import io.github.arrayv.main.ArrayVisualizer;
+import io.github.arrayv.sorts.templates.BogoSorting;
+
+//bubble bogo sort but stupider, picks a random item from 0 to n-1 and swaps it with the subsequent item. Unconditionally. Checks if array = sorted. If no, do it again.
  public final class BosoSort extends BogoSorting {
    public BosoSort(ArrayVisualizer arrayVisualizer) {
      super(arrayVisualizer);
