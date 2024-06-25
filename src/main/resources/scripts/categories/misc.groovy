08889593b188ae1b928c6fbe1d6aabf660e68f6c
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-24 14:04:27 -1000

    Update misc.groovy

--- src/main/resources/scripts/categories/misc.groovy
+++ src/main/resources/scripts/categories/misc.groovy
@@ -3,4 +3,5 @@ import io.github.arrayv.prompts.SortPrompt
 SortPrompt.setSortThreadForCategory('Miscellaneous Sorts', 2) {
     run PancakeSort go 128.numbers, 0.015.speed
     run BurntPancakeSort go 128.numbers, 0.015.speed
+    run HalfSorting go 128.numbers, 0.05
 }
