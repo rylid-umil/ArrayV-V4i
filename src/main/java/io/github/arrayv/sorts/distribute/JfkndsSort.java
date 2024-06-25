2c56f82d80091aec8e5bf7cc74fd4f350bcfd491
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-24 19:40:19 -1000

    why

--- src/main/java/io/github/arrayv/sorts/distribute/JfkndsSort.java
+++ src/main/java/io/github/arrayv/sorts/distribute/JfkndsSort.java
@@ -6,26 +6,29 @@ import io.github.arrayv.sorts.templates.BogoSorting;
 //literally sets random integers from 0 to max. if its sorted, good. Probably will
 //NEVER result in the same data distribution as the previous.
 
-public final class JfkndsSort extends BogoSorting {
-  public JfkndsSort(ArrayVisualizer arrayvisualizer) {
-    super(arrayVisualizer);
-    this.setSortListName("Jfknds");
-    this.setRunAllSortsName("Jfknds Sort");
-    this.setRunSortName("Jfkndssort");
-    this.setCategory("Impractical Sorts");
-    this.setComparisonBased(true);
-    this.setBucketSort(false);
-    this.setRadixSort(false);
-    this.setUnreasonablySlow(true);
-    this.setUnreasonableLimit(13);
-    this.setBogoSort(true);
-  };
+public final class Jfknds extends BogoSorting {
+     public JfkndsSort(ArrayVisualizer arrayVisualizer) {
+         super(arrayVisualizer);
+         
+         this.setSortListName("Jfknds");
+         this.setRunAllSortsName("Jfknds Sort");
+         this.setRunSortName("Jfkndssort");
+         this.setCategory("Impractical Sorts");
+         this.setComparisonBased(true);
+         this.setBucketSort(false);
+         this.setRadixSort(false);
+         this.setUnreasonablySlow(true);
+         this.setUnreasonableLimit(13);
+         this.setBogoSort(true);
+  } 
+  
+  @Override
   public void runSort(int[] array, int length, int bucketCount) {
     int min = Reads.analyzeMin(array, length, 0.05, true);
     int max = Reads.analyzeMax(array, length, 0.05, true);
     while (!this.isArraySorted(array, length)) {
       for (int i = 0; i < length; i++) {
-        Writes.write(array, i, BogoSorting.randomInt(min, max), this.delay, true, false);
+        Writes.write(array, i, BogoSorting.ranInt(min, max), this.delay, true, false);
         Highlights.markArray(1, i);
       };
     };
