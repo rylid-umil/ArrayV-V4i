b2ec86b6fb650886fae223f1ee4e9452344dba98
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-25 13:40:55 -1000

    Update JfkndsSort.java

--- src/main/java/io/github/arrayv/sorts/misc/JfkndsSort.java
+++ src/main/java/io/github/arrayv/sorts/misc/JfkndsSort.java
@@ -1,4 +1,4 @@
-package io.github.arrayv.sorts.misc;
+package io.github.arrayv.sorts.distribute;
 
 import io.github.arrayv.main.ArrayVisualizer;
 import io.github.arrayv.sorts.templates.BogoSorting;
@@ -6,14 +6,14 @@ import io.github.arrayv.sorts.templates.BogoSorting;
 //literally sets random integers from 0 to max. if its sorted, good. Probably will
 //NEVER result in the same data distribution as the previous.
 
-public final class Jfknds extends BogoSorting {
+public final class JfkndsSort extends BogoSorting {
      public JfkndsSort(ArrayVisualizer arrayVisualizer) {
          super(arrayVisualizer);
          
          this.setSortListName("Jfknds");
          this.setRunAllSortsName("Jfknds Sort");
          this.setRunSortName("Jfkndssort");
-         this.setCategory("Miscellaneous Sorts");
+         this.setCategory("Impractical Sorts");
          this.setComparisonBased(true);
          this.setBucketSort(false);
          this.setRadixSort(false);
@@ -24,11 +24,11 @@ public final class Jfknds extends BogoSorting {
   
   @Override
   public void runSort(int[] array, int length, int bucketCount) {
-    int min = Reads.analyzeMin(array, length, this.delay, true);
-    int max = Reads.analyzeMax(array, length, this.delay, true);
+    int min = Reads.analyzeMin(array, length, 0.05, true);
+    int max = Reads.analyzeMax(array, length, 0.05, true);
     while (!this.isArraySorted(array, length)) {
       for (int i = 0; i < length; i++) {
-        Writes.write(array, i, BogoSorting.ranInt(min, max), this.delay, true, false);
+        Writes.write(array, i, BogoSorting.randInt(min, max), this.delay, true, false);
         Highlights.markArray(1, i);
       };
     };
