751521ea5c5a5d3339dced0450800c523b23e530
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-24 20:03:26 -1000

    New template

--- template.java
+++ template.java
@@ -1,19 +1,28 @@
-package sorts.category;
+package io.github.arrayv.sorts.exchange;
 
 import io.github.arrayv.main.ArrayVisualizer;
-import io.github.arrayv.sortdata.SortMeta;
 import io.github.arrayv.sorts.templates.Sort;
 
-@SortMeta(
-    name = "My",
-    category = "Category Sorts"
-)
-public final class MySort extends Sort {
-    public MySort(ArrayVisualizer arrayVisualizer) {
+/*
+ *
+Optional sort explanation
+ *
+ */
+public final class NewSort extends Sort {
+    public NewSort(ArrayVisualizer arrayVisualizer) {
         super(arrayVisualizer);
+
+        this.setSortListName("New");
+        this.setRunAllSortsName("New Sort");
+        this.setRunSortName("Newsort");
+        this.setCategory("Uncategorized");
+        this.setBucketSort(false);
+        this.setRadixSort(false);
+        this.setUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.setBogoSort(true);
     }
 
-    @Override
-    public void runSort(int[] array, int currentLength, int bucketCount) {
+    public void runSort(int[] array, int length, int bucketCount) {
     }
 }
