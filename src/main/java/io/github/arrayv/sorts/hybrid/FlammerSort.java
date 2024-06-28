91f07e2b9478855ad4a1e65aad9a59144e26c957
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-28 10:05:58 -1000

    Create FlammerSort.java

---
+++ src/main/java/io/github/arrayv/sorts/hybrid/FlammerSort.java
@@ -0,0 +1,29 @@
+package io.github.arrayv.sorts.hybrid;
+
+import io.github.arrayv.main.ArrayVisualizer;
+import io.github.arrayv.sorts.templates.FlammerSorting;
+
+/*
+ *
+  An out of place variant of Lazy Stable Sort with a few additions. Another programmer reading this i want to know the time complexity-
+ *
+ */
+public final class FlammerSort extends FlammerSorting {
+    public FlammerSort(ArrayVisualizer arrayVisualizer) {
+        super(arrayVisualizer);
+
+        this.setSortListName("Flammer");
+        this.setRunAllSortsName("Rylid's Flammer Sort");
+        this.setRunSortName("Flammersort");
+        this.setCategory("Hybrid Sorts");
+        this.setBucketSort(false);
+        this.setRadixSort(false);
+        this.setUnreasonablySlow(false);
+        this.setUnreasonableLimit(0);
+        this.setBogoSort(false);
+    }
+
+    public void runSort(int[] array, int length, int bucketCount) {
+        this.flammerSort(array, length, 0.33);
+    }
+}
