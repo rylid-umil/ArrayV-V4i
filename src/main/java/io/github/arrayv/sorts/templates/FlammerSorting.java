6b30eb6914569f132834f068f90a10c34625a3c9
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-28 10:02:50 -1000

    oopsiedoosies()

--- src/main/java/io/github/arrayv/sorts/templates/FlammerSorting.java
+++ src/main/java/io/github/arrayv/sorts/templates/FlammerSorting.java
@@ -8,8 +8,9 @@ public abstract class FlammerSorting extends Sort {
     protected FlammerSorting(ArrayVisualizer arrayVisualizer) {
         super(arrayVisualizer);
     };
-    private void flammerMerge(int[] array, int[] tmp, int start, int mid, int end) {
-        if (start==end) {
+    private void flammerMerge(int[] array, int[] tmp, int start, int mid, int end, double delay) {
+        if (start==end) return;
+        if (end - start < 32) {
             return;
         }
         else if (end - start < 64) {
@@ -48,7 +49,7 @@ public abstract class FlammerSorting extends Sort {
                             newMid = finderA + 1;
                         };
                         finderA++;
-                        Delays.sleep(0.1);
+                        Delays.sleep(delay);
                     };
                     if (finderA > end) {
                         done = true;
@@ -63,18 +64,19 @@ public abstract class FlammerSorting extends Sort {
                             compBFound = true;
                             compB = finderB;
                         };
-                        finderB++'
+                        finderB++;
+                        Delays.sleep(delay);
                     };
                 };
                 if (done) break;
-                Writes.write(tmp, compB + 1, array[compB + 1], 0.1, true, true);
+                Writes.write(tmp, compB + 1, array[compB + 1], delay, true, true);
                 int j = compB;
                 int h = -1;
                 for(int i = start; i < newMid - 1; i++) {
                     Highlights.markArray(1, i);
                     Highlights.markArray(2, compB);
                     if (Reads.compareValues(array[i], array[compB]) == 1) {
-                        Writes.write(tmp, j, array[i], 0.1, true, true);
+                        Writes.write(tmp, j, array[i], delay, true, true);
                         j--;
                         if (h == -1); {
                             h = i;
@@ -84,13 +86,32 @@ public abstract class FlammerSorting extends Sort {
                 };
                 for(int k = compB; k > compA; k--) {
                     Highlights.markArray(1, k);
-                    Writes.write(tmp, j, array[k], 0.1, true, true);
+                    Writes.write(tmp, j, array[k], delay, true, true);
                     j--;
                 };
                 for(int l = h; l < compB; l++) {
-                    Writes.write(array, l, tmp[l], 0.1, true, false);
+                    Writes.write(array, l, tmp[l], delay, true, false);
                 };
             };
         };
     };
+  protected void flammerSort(int[] array, int length, double delay) {
+        binaryInserter = new BinaryInsertionSort(this.arrayVisualizer);
+
+        if(length < 32) {
+            binaryInserter.customBinaryInsert(array, 0, length, 0.333);
+            return;
+        }
+
+        int[] tmp = Writes.createExternalArray(length);
+
+        int start = 0;
+        int end = length;
+        int mid = start + ((end - start) / 2);
+
+        flammerMerge(array, tmp, start, mid, end, delay);
+
+        Writes.deleteExternalArray(tmp);
+    }
+}
 };
