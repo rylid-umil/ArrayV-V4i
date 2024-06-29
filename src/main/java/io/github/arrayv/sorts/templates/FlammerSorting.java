8bc39f085fbf0558a81b12bf07d736e98d00a1bd
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-06-29 00:02:36 -1000

    fix

--- src/main/java/io/github/arrayv/sorts/templates/FlammerSorting.java
+++ src/main/java/io/github/arrayv/sorts/templates/FlammerSorting.java
@@ -1,4 +1,4 @@
-package io.github.arrayv.sorts.templates
+package io.github.arrayv.sorts.templates;
   
 import io.github.arrayv.main.ArrayVisualizer;
 import io.github.arrayv.sorts.insert.BinaryInsertionSort;
@@ -17,8 +17,8 @@ public abstract class FlammerSorting extends Sort {
             binaryInserter.customBinaryInsert(array, start, end, 0.333);
         }
         else {
-            flammerMerge(array, tmp, start, (mid+start)/2, mid);
-            flammerMerge(array, tmp, mid, (mid+end)/2, end);
+            flammerMerge(array, tmp, start, (mid+start)/2, mid, delay);
+            flammerMerge(array, tmp, mid, (mid+end)/2, end, delay);
 
             int low = start;
             int high = end;
@@ -27,70 +27,90 @@ public abstract class FlammerSorting extends Sort {
             int finderA = start;
             int finderB = end;
             int newMid = mid;
-            int done = false;
+			int w = 0;
+			int st = start;
+            boolean done = false;
             while(!done) {
                 boolean compAFound = false;
                 boolean compBFound = false;
                 compA = start;
                 compB = end;
                 finderA = start;
-                finderB = end;
+                finderB = end - 1;
                 newMid = mid;
                 done = false;
-                while (!compAFound || !compBFound) {
+                while (true) {
                     if (!compAFound) {
                         Highlights.markArray(1, finderA);
                         Highlights.markArray(2, finderA + 1);
                         if (Reads.compareValues(array[finderA], array[finderA + 1]) == 1) {
                             Highlights.clearMark(1);
-                            Highlights.clearMarh(2);
+                            Highlights.clearMark(2);
                             compAFound = true;
                             compA = finderA;
                             newMid = finderA + 1;
                         };
                         finderA++;
                         Delays.sleep(delay);
+						if (finderA > (end - 1)) done = true;
+						if (compAFound) break;
                     };
-                    if (finderA > end) {
+                    if (finderA > (end - 1)) {
                         done = true;
                         break;
                     }
-                    if (!compBFound && compAFound) {
+				}
+                if (done) break;
+				while (true) {
+                    if ((!compBFound) && compAFound) {
                         Highlights.markArray(3, compA);
                         Highlights.markArray(4, finderB);
-                        if (Reads.compareValues(array[compA], array[finderB) {
+                        if (Reads.compareValues(array[compA], array[finderB]) == 1) {
                             Highlights.clearMark(3);
                             Highlights.clearMark(4);
                             compBFound = true;
                             compB = finderB;
                         };
-                        finderB++;
+                        finderB--;
                         Delays.sleep(delay);
+						if (compBFound) break;
                     };
                 };
-                if (done) break;
-                Writes.write(tmp, compB + 1, array[compB + 1], delay, true, true);
+				if (compB < end - 1) {
+					Writes.write(tmp, compB + 1, array[compB + 1], delay, true, true);
+				}
+				else {
+					compB = end - 1;
+				};
                 int j = compB;
-                int h = -1;
-                for(int i = start; i < newMid - 1; i++) {
-                    Highlights.markArray(1, i);
+                int h = end - 1;
+				boolean seth = false;
+                for(int isc = start; isc < newMid; isc++) {
+                    Highlights.markArray(1, isc);
                     Highlights.markArray(2, compB);
-                    if (Reads.compareValues(array[i], array[compB]) == 1) {
-                        Writes.write(tmp, j, array[i], delay, true, true);
+                    if (Reads.compareValues(array[isc], array[compB]) == 1) {
+                        Writes.write(tmp, j, array[isc], delay, true, true);
                         j--;
-                        if (h == -1); {
-                            h = i;
+                        if (isc < h) {
+                            h = isc;
+							seth = true;
                             Highlights.markArray(3, h);
                         }
                     };
+					Delays.sleep(0.4);
                 };
+				Writes.reversal(tmp, j + 1, compB, 0.1, true, true);
                 for(int k = compB; k > compA; k--) {
                     Highlights.markArray(1, k);
                     Writes.write(tmp, j, array[k], delay, true, true);
                     j--;
                 };
-                for(int l = h; l < compB; l++) {
-                    Writes.write(array, l, tmp[l], delay, true, false);
+				w = j + 1;
+                for(int l = h; l < compB + 1; l++) {
+					if (l < end) {
+                    Writes.write(array, l, tmp[w], delay, true, false);
+					w++;
+					};
                 };
             };
         };
@@ -101,7 +121,7 @@ public abstract class FlammerSorting extends Sort {
         if(length < 32) {
             binaryInserter.customBinaryInsert(array, 0, length, 0.333);
             return;
-        }
+        };
 
         int[] tmp = Writes.createExternalArray(length);
 
@@ -112,6 +132,5 @@ public abstract class FlammerSorting extends Sort {
         flammerMerge(array, tmp, start, mid, end, delay);
 
         Writes.deleteExternalArray(tmp);
-    }
-}
+    };
 };
