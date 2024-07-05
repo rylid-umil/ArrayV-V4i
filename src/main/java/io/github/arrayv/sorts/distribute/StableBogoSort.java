e4d36864b4f684a91637ba32ace653ad6d3bc271
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-07-05 13:36:16 -1000

    Create StableBogoSort.java

---
+++ src/main/java/io/github/arrayv/sorts/distribute/StableBogoSort.java
@@ -0,0 +1,29 @@
+package io.github.arrayv.sorts.distribute;
+
+import io.github.arrayv.main.ArrayVisualizer;
+import io.github.arrayv.sorts.templates.BogoSorting;
+
+/**
+ * A variation of Bogo Sort that scans between the two to-be-swapped items, changing the swap points if it finds identicals until the two collide
+ */
+public final class BogoSort extends BogoSorting {
+    public BogoSort(ArrayVisualizer arrayVisualizer) {
+        super(arrayVisualizer);
+
+        this.setSortListName("Stable Bogo");
+        this.setRunAllSortsName("Stable Bogo Sort");
+        this.setRunSortName("Stable Bogosort");
+        this.setCategory("Impractical Sorts");
+        this.setBucketSort(false);
+        this.setRadixSort(false);
+        this.setUnreasonablySlow(true);
+        this.setUnreasonableLimit(8);
+        this.setBogoSort(true);
+    }
+
+    @Override
+    public void runSort(int[] array, int length, int bucketCount) {
+        while(!this.isArraySorted(array, length))
+            this.stableBogoSwap(array, 0, length, false);
+    }
+}
