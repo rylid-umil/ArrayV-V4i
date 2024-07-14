aaa8f0266655994334795d2c511121c78aae50d9
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-07-13 17:43:45 -1000

    crashes via stack underflow

---
+++ src/main/java/io/github/arrayv/sorts/joke/OmegaFaithSort.java
@@ -0,0 +1,26 @@
+package io.github.arrayv.sorts.exchange;
+
+import io.github.arrayv.main.ArrayVisualizer;
+import io.github.arrayv.sorts.templates.Sort;
+
+// "The list is sorted in a way you cannot comprehend" - wise words
+
+public final class OmegaNewSort extends Sort {
+    public OmegaNewSort(ArrayVisualizer arrayVisualizer) {
+        super(arrayVisualizer);
+
+        this.setSortListName("Omega Faith");
+        this.setRunAllSortsName("Omega Faith Sort");
+        this.setRunSortName("Omega Faithsort");
+        this.setCategory("Joke Sorts");
+        this.setBucketSort(false);
+        this.setRadixSort(false);
+        this.setUnreasonablySlow(true);
+        this.setUnreasonableLimit(1);
+        this.setBogoSort(false);
+    }
+
+    public void runSort(int[] array, int length, int bucketCount) {
+        this.runSort(array, length, bucketCount);
+    }
+}
