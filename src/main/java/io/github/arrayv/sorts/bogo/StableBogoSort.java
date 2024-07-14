1f6b1a3ac10b0a69c579f8ea5a74994494d972ee
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-07-13 18:02:29 -1000

    Update StableBogoSort.java

--- src/main/java/io/github/arrayv/sorts/bogo/StableBogoSort.java
+++ src/main/java/io/github/arrayv/sorts/bogo/StableBogoSort.java
@@ -1,4 +1,4 @@
-package io.github.arrayv.sorts.distribute;
+package io.github.arrayv.sorts.bogo;
 
 import io.github.arrayv.main.ArrayVisualizer;
 import io.github.arrayv.sorts.templates.BogoSorting;
@@ -13,7 +13,7 @@ public final class StableBogoSort extends BogoSorting {
         this.setSortListName("Stable Bogo");
         this.setRunAllSortsName("Stable Bogo Sort");
         this.setRunSortName("Stable Bogosort");
-        this.setCategory("Impractical Sorts");
+        this.setCategory("Bogo Sorts");
         this.setBucketSort(false);
         this.setRadixSort(false);
         this.setUnreasonablySlow(true);
