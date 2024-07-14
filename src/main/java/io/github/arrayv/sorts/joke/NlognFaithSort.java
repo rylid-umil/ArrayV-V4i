34e339784d6772c3857c8dfe7a2e5a045e43b3bd
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-07-13 17:41:09 -1000

    recursive faith

---
+++ src/main/java/io/github/arrayv/sorts/joke/NlognFaithSort.java
@@ -0,0 +1,30 @@
+package io.github.arrayv.sorts.joke;
+
+import io.github.arrayv.main.ArrayVisualizer;
+import io.github.arrayv.sorts.templates.Sort;
+
+// "The list is sorted in a way you cannot comprehend" - wise words
+
+public final class NlognFaithSort extends Sort {
+    public NlognFaithSort(ArrayVisualizer arrayVisualizer) {
+        super(arrayVisualizer);
+
+        this.setSortListName("Nlogn Faith");
+        this.setRunAllSortsName("Nlogn Faith Sort");
+        this.setRunSortName("Nlogn Faithsort");
+        this.setCategory("Joke Sorts");
+        this.setBucketSort(false);
+        this.setRadixSort(false);
+        this.setUnreasonablySlow(true);
+        this.setUnreasonableLimit(1);
+        this.setBogoSort(false);
+    }
+    public void faith(int array, int start, int end) {
+        if (start == end) break;
+        this.faith(array, start, ((start + end) / 2) - 1);
+        this.faith(array, (start + end / 2), end);
+    };
+    public void runSort(int[] array, int length, int bucketCount) {
+        this.faith(array, 0, length - 1);
+    }
+}
