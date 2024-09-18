c602d2c56b31957851f751048fc9a449db71b437
rylid-umil <81439905+rylid-umil@users.noreply.github.com>
2024-09-17 16:23:40 -1000

    Credit to YM for this
    https://discord.com/channels/592082838791127075/1207445887303815168/1285654731225694319

--- src/main/java/io/github/arrayv/visuals/bars/SineWave.java
+++ src/main/java/io/github/arrayv/visuals/bars/SineWave.java
@@ -1,14 +1,14 @@
-package io.github.arrayv.visuals.bars;
+package visuals.bars;
 
-import io.github.arrayv.main.ArrayVisualizer;
-import io.github.arrayv.utils.Highlights;
-import io.github.arrayv.utils.Renderer;
-import io.github.arrayv.visuals.Visual;
+import java.awt.Color;
 
-import java.awt.*;
+import main.ArrayVisualizer;
+import utils.Highlights;
+import utils.Renderer;
+import visuals.Visual;
 
 /*
- *
+ * 
 MIT License
 
 Copyright (c) 2020 MusicTheorist
@@ -34,44 +34,101 @@ SOFTWARE.
  *
  */
 
-public final class SineWave extends Visual {
+final public class SineWave extends Visual {
 
-    public SineWave(ArrayVisualizer arrayVisualizer) {
-        super(arrayVisualizer);
+    public SineWave(ArrayVisualizer ArrayVisualizer) {
+        super(ArrayVisualizer);
     }
+	
+	private final int X_MARGIN = 40;
+	private final int Y_OFFSET = 70;
+	
+	private final double B_SCALE = 1.05;
+	
+	private void bubble(ArrayVisualizer ArrayVisualizer, Highlights Highlights, int[] array, int N, int xa, int xb, int y, int a, int b) {
+		int s = (int)((xb-xa) * B_SCALE);
+		
+		if(s < 2) return;
+		
+		int x = (xa+xb)/2, n = b-a, la = 90;
+		
+		for(int i = a; i < b; i++) {
+			int la1 = 90 - (int)(360d/n * (i-a+1));
+			
+			if(la1 == la) continue;
+			
+			this.mainRender.setColor(Color.WHITE);
+			
+			if(!Highlights.containsPosition(i))
+				this.mainRender.setColor(getIntColor(array[i], N));
+			
+			if(Highlights.fancyFinishActive() && i < Highlights.getFancyFinishPosition())
+				this.mainRender.setColor(Color.GREEN);
+			
+			this.mainRender.fillArc(x-s/2, y-s/2, s, s, la, la1-la);
+			la = la1;
+		}
+	}
+	
+	private void bubble2(ArrayVisualizer ArrayVisualizer, Highlights Highlights, int[] array, int N, int xa, int xb, int y0, int a, int b) {
+		int r = (int)((xb-xa) / 2d * B_SCALE);
+		
+		if(r < 1) return;
+		
+		int n = b-a;
+		double is = (double)N/n;
+		
+		int[] x = {xb-r, 0, 0};
+		int[] y = {y0, 0, 0};
+		
+		double disp = (1 + Math.cos((Math.PI * (array[b-1] - (N-1))) / (N * 0.5))) * 0.5;
+		x[2] = x[0] + (int)(disp * r * Math.cos(Math.PI * (2d*(n-1) / n - 0.5)));
+		y[2] = y[0] + (int)(disp * r * Math.sin(Math.PI * (2d*(n-1) / n - 0.5)));
+		
+		for(int i = 0; i < n; i++) {
+			x[1] = x[2];
+			y[1] = y[2];
+			
+			disp = (1 + Math.cos((Math.PI * (array[a+i] - (int)(i*is))) / (N * 0.5))) * 0.5;
+			x[2] = x[0] + (int)(disp * r * Math.cos(Math.PI * (2d*i / n - 0.5)));
+			y[2] = y[0] + (int)(disp * r * Math.sin(Math.PI * (2d*i / n - 0.5)));
+			
+			if(Highlights.fancyFinishActive() && a+i < Highlights.getFancyFinishPosition())
+				this.mainRender.setColor(Color.GREEN);
+				
+			else if(Highlights.containsPosition(a+i)) {
+				this.mainRender.setColor(ArrayVisualizer.getHighlightColor());
+				this.extraRender.drawPolygon(x, y, 3); 
+			}
+			else if(ArrayVisualizer.colorEnabled()) 
+				this.mainRender.setColor(getIntColor(array[a+i], ArrayVisualizer.getCurrentLength()));
+			
+			else this.mainRender.setColor(Color.WHITE);
+			
+			this.mainRender.fillPolygon(x, y, 3); 
+		}
+		this.extraRender.setStroke(ArrayVisualizer.getDefaultStroke());
+	}
 
     @Override
-    public void drawVisual(int[] array, ArrayVisualizer arrayVisualizer, Renderer renderer, Highlights Highlights) {
-        for (int i = 0, j = 0; i < renderer.getArrayLength(); i++) {
-            if (Highlights.fancyFinishActive() && i < Highlights.getFancyFinishPosition())
-                this.mainRender.setColor(Color.GREEN);
-
-            else if (arrayVisualizer.colorEnabled())
-                this.mainRender.setColor(getIntColor(array[i], arrayVisualizer.getCurrentLength()));
-
-            else this.mainRender.setColor(Color.WHITE);
-
-            int width = (int) (renderer.getXScale() * (i + 1)) - j;
-
-            int y = (int) (((renderer.getViewSize() - 20) / 2.5) * Math.sin((2 * Math.PI * ((double) array[i] / renderer.getArrayLength()))) + renderer.halfViewSize() - 20);
-            this.mainRender.fillRect(j + 20, renderer.getYOffset() + y, Math.max(width, 1), 20);
-
-            j += width;
-        }
-        this.mainRender.setColor(arrayVisualizer.getHighlightColor());
-
-        for (int i = 0, j = 0; i < renderer.getArrayLength(); i++) {
-            int width = (int) (renderer.getXScale() * (i + 1)) - j;
-
-            if (Highlights.containsPosition(i)) {
-                int y = (int) (((renderer.getViewSize() - 20) / 2.5) * Math.sin((2 * Math.PI * ((double) array[i] / renderer.getArrayLength()))) + renderer.halfViewSize() - 20);
-                this.mainRender.fillRect(j + 20, renderer.getYOffset() + y, Math.max(width, 2), 20);
-            }
-            j += width;
-        }
-        if (arrayVisualizer.externalArraysEnabled()) {
-            this.mainRender.setColor(Color.BLUE);
-            this.mainRender.fillRect(0, renderer.getYOffset() + renderer.getViewSize() - 20, arrayVisualizer.currentWidth(), 1);
-        }
+    public void drawVisual(int[] array, ArrayVisualizer ArrayVisualizer, Renderer Renderer, Highlights Highlights) {
+		if(Renderer.auxActive) return;
+		
+		int w = ArrayVisualizer.windowWidth(),
+		    h = ArrayVisualizer.windowHeight() - Y_OFFSET,
+		    s = Math.min(w, h) - X_MARGIN,
+			y = Y_OFFSET + s/2,
+		    N = ArrayVisualizer.getCurrentLength();
+			
+		int lm = w/2 - s/2;
+		int j = 0, lx = lm;
+		
+		for(int i = 1; i < N; i++) {
+			if(array[i] < array[i-1]) {
+				int x = lm + (int)((double)i/N * s);
+				this.bubble2(ArrayVisualizer, Highlights, array, N, lx, lx = x, y, j, j = i);
+			}
+		}
+		this.bubble2(ArrayVisualizer, Highlights, array, N, lx, lm+s, y, j, N);
     }
 }
