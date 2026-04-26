26fc422867e5da3bc7085a50e33427b96ea6d161
Rylid-Umil <81439905+rylid-umil@users.noreply.github.com>
2026-04-25 18:04:55 -0700

    42

--- src/main/resources/scripts/categories/impractical.groovy
+++ src/main/resources/scripts/categories/impractical.groovy
@@ -1,17 +1,17 @@
 import io.github.arrayv.prompts.SortPrompt
 
-SortPrompt.setSortThreadForCategory('Impractical Sorts', 32) {
+SortPrompt.setSortThreadForCategory('Impractical Sorts', 29) {
     run BadSort go 64.numbers, 0.0075.speed
+    run RodSort go 64.numbers, 0.0055.speed
     run StoogeSort go 64.numbers, 0.005.speed
     run QuadStoogeSort go 64.numbers, 0.005.speed
     run SillySort go 64.numbers, 0.5.speed
     run SlowSort go 64.numbers, 0.5.speed
     run SnuffleSort go 64.numbers, 0.25.speed
     run HanoiSort go 8.numbers, 0.025.speed
+    run YeetSort go 64.numbers
 
     // Bogosorts
-    def oldSofterSounds = arrayv.sounds.softerSounds
-    arrayv.sounds.softerSounds = true
     // The not-bad ones
     run SelectionBogoSort go 64.numbers, 1e-9.speed
     run BubbleBogoSort go 40.numbers, 1e-9.speed
@@ -24,15 +24,16 @@ SortPrompt.setSortThreadForCategory('Impractical Sorts', 32) {
     run MergeBogoSort go 9.numbers, 1e-9.speed
     run SmartGuessSort go 8.numbers, 1e-9.speed
     // The scary ones
-    run BozoSort go 7.numbers, 1e-9.speed
     run DeterministicBogoSort go 7.numbers, 1e-9.speed
     run SmartBogoBogoSort go 6.numbers, 1e-9.speed
-    run BosoSort go 6.numbers, 1e-9.speed
     run BogoSort go 6.numbers, 1e-9.speed
+    run BozoSort go 7.numbers, 1e-9.speed
+    run BosoSort go 6.numbers, 1e-9.speed
+    run BomoSort go 6.numbers, 1e-9.speed
+    run BovoSort go 6.numbers, 1e-9.speed
     run OptimizedGuessSort go 5.numbers, 1e-9.speed
     run RandomGuessSort go 5.numbers, 1e-9.speed
     run GuessSort go 4.numbers, 1e-9.speed
     // aaaaaa
     run BogoBogoSort go 4.numbers, 1e-9.speed
-    arrayv.sounds.softerSounds = oldSofterSounds
 }
