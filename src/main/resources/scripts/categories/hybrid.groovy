e0ed6d066aa2cdd6f28f840e5a0f2617e62e4503
Rylid-Umil <81439905+rylid-umil@users.noreply.github.com>
2026-04-25 17:47:33 -0700

    Update hybrid.groovy

--- src/main/resources/scripts/categories/hybrid.groovy
+++ src/main/resources/scripts/categories/hybrid.groovy
@@ -1,21 +1,27 @@
 import io.github.arrayv.prompts.SortPrompt
 import io.github.arrayv.utils.Shuffles
 
-SortPrompt.setSortThreadForCategory('Hybrid Sorts', 32) {
+SortPrompt.setSortThreadForCategory('Hybrid Sorts', 38) {
     run HybridCombSort go 1024.numbers
     run IntroCircleSortRecursive go 1024.numbers
     run IntroCircleSortIterative go 1024.numbers
+    run FlexCombSort go 1024 numbers, 130.buckets
     run BinaryMergeSort go 2048.numbers
     run MergeInsertionSort go 2048.numbers, 1.75.speed
     run WeaveMergeSort go 2048.numbers, ((arrayv.arrayManager.containsShuffle(Shuffles.RANDOM) ? 1.65 : 6.5).speed)
     run TimSort go 2048.numbers
+    run FlammerSort go 1024.numbers
+    run AdaptiveSort go 2048.numbers
     run CocktailMergeSort go 2048.numbers
     run LaziestSort go 1024.numbers
+    run LazierestSort go 1024.numbers
     run WikiSort go 2048.numbers
+    run ChaliceSort go 2048.numbers
     run GrailSort go 2048.numbers
     run AdaptiveGrailSort go 2048.numbers
     run UnstableGrailSort go 2048.numbers
     run SqrtSort go 2048.numbers
+    run SynchronousSqrtSort go 2048.numbers
     run KotaSort go 2048.numbers
     run EctaSort go 2048.numbers
     run ParallelBlockMergeSort go 2048.numbers
@@ -26,6 +32,7 @@ SortPrompt.setSortThreadForCategory('Hybrid Sorts', 32) {
     run MedianMergeSort go 2048.numbers
     run BufferPartitionMergeSort go 2048.numbers
     run IntroSort go 2048.numbers
+    run AttackSort go 2048.numbers
     run OptimizedBottomUpMergeSort go 2048.numbers
     run OptimizedDualPivotQuickSort go 2048.numbers, 0.75.speed
     run OptimizedWeaveMergeSort go 1024.numbers, 0.4.speed
