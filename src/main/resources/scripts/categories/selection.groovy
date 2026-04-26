3ca516d405e49fbb0d63dcddea718bcc5cddfd7a
Rylid-Umil <81439905+rylid-umil@users.noreply.github.com>
2026-04-25 18:16:22 -0700

    Update selection.groovy

--- src/main/resources/scripts/categories/selection.groovy
+++ src/main/resources/scripts/categories/selection.groovy
@@ -1,6 +1,6 @@
 import io.github.arrayv.prompts.SortPrompt
 
-SortPrompt.setSortThreadForCategory('Selection Sorts', 18) {
+SortPrompt.setSortThreadForCategory('Selection Sorts', 24) {
     run SelectionSort go 128.numbers, 0.01.speed
     run DoubleSelectionSort go 128.numbers, 0.01.speed
     run StableSelectionSort go 128.numbers, 0.5.speed
@@ -9,12 +9,20 @@ SortPrompt.setSortThreadForCategory('Selection Sorts', 18) {
     run BingoSort go 128.numbers, 0.1.speed
     run MaxHeapSort go 2048.numbers, 1.5.speed
     run MinHeapSort go 2048.numbers, 1.5.speed
+    run MinMaxHeapSort go 2048.numbers, 1.5.speed
     run FlippedMinHeapSort go 2048.numbers, 1.5.speed
+    run TernaryHeapSort go 2048.numbers
     run BaseNMaxHeapSort go 2048.numbers, 4.buckets, 1.5.speed
     run TriangularHeapSort go 2048.numbers, 1.5.speed
     run WeakHeapSort go 2048.numbers
-    run TernaryHeapSort go 2048.numbers
+    run OutOfPlaceHeapSort go 2048.numbers, 1.5.speed
+    run BottomUpHeap go 2048.numbers, 1.5.speed
+    run BinomialHeapSort go 2048.numbers
+    run LazyHeapSort go 2048.numbers, 1.5.speed
     run SmoothSort go 2048.numbers, 1.5.speed
     run PoplarHeapSort go 2048.numbers
+    run BinomialSmoothSort go 2048.numbers, 1.5 speed
+    run ClassicTournamentSort go 2048.numbers, 1.5 speed
     run TournamentSort go 2048.numbers, 1.5.speed
+    run AsynchronousSort go 2048.numbers
 }
