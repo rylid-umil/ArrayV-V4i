128e7c05e203d8b44d362a3c7c3d528e264d0ede
Rylid-Umil <81439905+rylid-umil@users.noreply.github.com>
2026-04-25 23:38:51 -0700

    Update magic.groovy

--- src/main/resources/scripts/categories/magic.groovy
+++ src/main/resources/scripts/categories/magic.groovy
@@ -2,11 +2,11 @@ import io.github.arrayv.prompts.SortPrompt
 import io.github.arrayv.utils.Shuffles
 
 SortPrompt.setSortThreadForCategory('Magic Sorts', 7) {
-    run StraighteningSort go 1024.numbers, 0.75.speed
-    run DrillStraighteningSort go 1024.numbers, 0.75.speed
-    run MagicBubbleSort go 256.numbers, 0.5.speed
-    run MagicBaiaiSort go 256.numbers, 0.5.speed
-    run MagicInsertionSort go 256.numbers, 0.5.speed
-    run MagicSelectionSort go 256.numbers, 1.speed
-    run OptimizedMagicSelectionSort go 256.numbers, 0.5.speed
+    run StraighteningSort go 1024.numbers
+    run DrillStraighteningSort go 1024.numbers
+    run MagicBubbleSort go 256.numbers
+    run MagicBaiaiSort go 256.numbers
+    run MagicInsertionSort go 256.numbers
+    run MagicSelectionSort go 256.numbers, 0.33.speed
+    run OptimizedMagicSelectionSort go 256.numbers
 }
